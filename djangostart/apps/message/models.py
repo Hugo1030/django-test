5461e0ac1c8ab0385a1ecffdf6c06afd6732d27b
Hugo1030 <yzhjsw520@gmail.com>
2017-11-12 15:46:37 +0800

    add boject_key primary_key

--- djangostart/apps/message/models.py
+++ djangostart/apps/message/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class UserMessage(models.Model):
-    name = models.CharField(max_length=20, verbose_name="用户名") # verbose 类似于注释
+    object_key = models.CharField(max_length=50, default="", primary_key=True, verbose_name="主键")
+    name = models.CharField(max_length=20, null=True, blank=True, default="", verbose_name="用户名") # verbose 类似于注释
     email = models.EmailField(verbose_name="邮箱")
     address = models.CharField(max_length=100, verbose_name="联系地址")
     message = models.CharField(max_length=500, verbose_name="留言信息")
