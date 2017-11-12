ba59abca37ee1501472fb1856c136c8c9b4b81f5
Hugo1030 <yzhjsw520@gmail.com>
2017-11-12 11:28:18 +0800

    create usermessage models

--- djangostart/apps/message/models.py
+++ djangostart/apps/message/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UserMessage(models.Model):
+    name = models.CharField(max_length=20, verbose_name="用户名") # verbose 类似于注释
+    email = models.EmailField(verbose_name="邮箱")
+    address = models.CharField(max_length=100, verbose_name="联系地址")
+    message = models.CharField(max_length=500, verbose_name="留言信息")
+
+    class Meta: # 元数据类型，「任何不适字段的数据」
+        verbose_name = "用户留言信息"
