d58e3144bd5c4cfc1d46d4e8b2fe970c3ddca4e3
Hugo1030 <yzhjsw520@gmail.com>
2017-11-12 20:18:45 +0800

    templates use urlname

--- djangostart/apps/message/views.py
+++ djangostart/apps/message/views.py
@@ -3,17 +3,11 @@ from .models import UserMessage
 
 # Create your views here.
 def getform(request):
-    all_messages = UserMessage.objects.filter(name='hugo', address='beijing')
-    for message in all_messages:
-        message.delete()
-    # user_message = UserMessage(name='hugo2', email='2@2.com', address='beijing2', message='HelloWorld2', object_id='HelloWorld2')
-    # user_message.save()
-    if request.method == 'POST':
-        name = request.POST.get('name', '')
-        address = request.POST.get('address', '')
-        email = request.POST.get('email', '')
-        message = request.POST.get('message', '')
-        user_message = UserMessage(name=name, email=email, address=address, message=message, object_id='HelloWorld3')
-        user_message.save()
+    message = None
+    all_messages = UserMessage.objects.filter(name='jane')
+    if all_messages:
+        message = all_messages[0]
 
-    return render(request, 'message_form.html')
+    return render(request, 'message_form.html', {
+        "my_message":message
+    })
