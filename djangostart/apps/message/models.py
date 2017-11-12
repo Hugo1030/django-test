4f7b27ef726595f3e84f9d57e43a251e00560db7
Hugo1030 <yzhjsw520@gmail.com>
2017-11-12 16:22:10 +0800

    ORM operations

--- djangostart/apps/message/models.py
+++ djangostart/apps/message/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class UserMessage(models.Model):
-    object_key = models.CharField(max_length=50, default="", primary_key=True, verbose_name="主键")
+    object_id = models.CharField(max_length=50, default="", primary_key=True, verbose_name="主键")
     name = models.CharField(max_length=20, null=True, blank=True, default="", verbose_name="用户名") # verbose 类似于注释
     email = models.EmailField(verbose_name="邮箱")
     address = models.CharField(max_length=100, verbose_name="联系地址")
@@ -10,3 +10,4 @@ class UserMessage(models.Model):
 
     class Meta: # 元数据类型，「任何不适字段的数据」
         verbose_name = "用户留言信息"
+        verbose_name_plural = verbose_name
--- djangostart/apps/message/views.py
+++ djangostart/apps/message/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import UserMessage
 
 # Create your views here.
 def getform(request):
+    all_messages = UserMessage.objects.all()
+    for message in all_messages:
+        print(message.address)
     return render(request, 'message_form.html')
